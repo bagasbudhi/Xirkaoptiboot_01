33098e42502d69032eb7f38c90752ad643b337a5
Bagas Budhi Permana <Bagaspermana88@gmail.com>
2021-01-22 14:32:30 +0700

    Update Xirkaoptiboot.ino
    Update jumpstart bootloader; add download.py on FLM folder Ardunesia

--- Xirkaoptiboot.ino
+++ Xirkaoptiboot.ino
@@ -1,7 +1,4 @@
-//uint16_t charSet[]= {65000, 32796, 16843, 10, 11234};
-//uint16_t displayInt;
-//int k;
-//char ch;
+
 
 //-xirka optiboot--
 
@@ -14,44 +11,22 @@
 #define SIGNATURE_1 0x95
 #define SIGNATURE_2 0x0f
 
-//#define MAKESTR(a) #a
-//#define MAKEVER(a, b) MAKESTR(a*256+b
 
-//asm(" .section .version\n"
-//    "optiboot_version: .word " MAKEVER(OPTIBOOT_MAJVER, OPTIBOOT_MINVER) "\n"
-//    " .section .text\n");
-
-//#include <inttype.h>
-//#include <avr/io.h>
-//#include <avr/pgmspace.h>
-
-//#include "boot.h"
-//#include "pin_defs.h"
 #include "stk500.h"
-//#include "random.h"
+#include <wdt.h>
 
-
-//#define F_CPU 8000000L
-//#define F_CPU FOSC
 #define BAUD_RATE 115200L
 
-//#if (F_CPU/BAUD_RATE) > 280
-//#ifndef SOFT_UART
-//#define SOFT_UART
-//#endif
-//#endif
 
 //uint8_t flash[(32UL<<10)];
 #define flash ((volatile uint8_t*)0x0)
 uint8_t buff[256];
 
+#define svc(code) asm volatile ("svc %[immediate]"::[immediate] "I" (code))
 void putch(char);
 uint8_t getch(void);
 //static inline void watchdogReset();
 //void watchdogConfig(uint8_t x);
-//#ifdef SOFT_UART
-//void uartDelay() __attribute__ ((naked));
-//#endif
 
 //void appStart() __attribute__ ((naked));
 
@@ -96,24 +71,14 @@ void __attribute__ ((long_call, section(".data.data_begin"))) setup(void) {
   
   Serial.begin(BAUD_RATE);
   Serial2.begin(115200);
-  delay(1000);
+  //delay(1);
   Serial2.println("hello");
+
+  wdt_enable(WDTO_2S);
   
   for(;;) {
 
     ch = getch();
-    
-//    if(ch == 'STK_NODEVICE' ){
-//      Serial.print("I received: STK_NODEVICE ");
-//      //Serial.println(ch);
-//      for(k=0;k<5;k++){
-//        displayInt = charSet[k]; 
-//        Serial.println(displayInt);
-//        
-//        }
-//        Serial.println();
-//      
-//      }
     
     if(ch == STK_GET_PARAMETER) { //0x41 'A'
       //Serial.print("Get parameter");
@@ -184,32 +149,12 @@ void __attribute__ ((long_call, section(".data.data_begin"))) setup(void) {
          flash[address] = 0x30;
          FLASHCTRL->CONTROL &= ~FLASH_CTRL_INTERASE_Msk;        // Disable erase mode
          while(!(FLASHCTRL->STATUS & FLASH_STAT_INTREADY_Msk)); // Wait until erase completed
-         //for(volatile int i=0; i<0xFFFFF; i++);
          __enable_irq();
          Serial2.println(" !");
        }
      
 
      verifySpace();
-
-//      #ifdef VIRTUAL_BOOT_PARTITION
-//      if ((uint16_t)(void*)address == 0) {
-//        // This is the reset vector page. We need to live-patch the code so the
-//        // bootloader runs.
-//        //
-//        // Move RESET vector to WDT vector
-//        uint16_t vect = buff[0] | (buff[1]<<8);
-//        rstVect = vect;
-//        wdtVect = buff[8] | (buff[9]<<8);
-//        vect -= 4; // Instruction is a relative jump (rjmp), so recalculate.
-//        buff[8] = vect & 0xff;
-//        buff[9] = vect >> 8;
-//
-//        // Add jump to bootloader at RESET vector
-//        buff[0] = 0x7f;
-//        buff[1] = 0xce; // rjmp 0x1d00 instruction
-//      }
-//      #endif
 
       // Write to flash
       Serial2.print("P 0x");
@@ -221,7 +166,6 @@ void __attribute__ ((long_call, section(".data.data_begin"))) setup(void) {
       ch = length; //Pagesize / 2;
       do{
         // Program
-//        
         *addrPtr = *bufPtr;
        while(!(FLASHCTRL->STATUS & FLASH_STAT_INTREADY_Msk)); // Wait until ready
         bufPtr++;
@@ -239,15 +183,14 @@ void __attribute__ ((long_call, section(".data.data_begin"))) setup(void) {
 
       verifySpace();
       
-//        Serial.println("Disabling Cache...");
-//        __disable_irq();
-//        if((CMSDK_CACHE->SR & CMSDK_CACHE_SR_CS_Msk) == (0b10 << CMSDK_CACHE_SR_CS_Pos))
-//        CMSDK_CACHE->CCR &=  ~CMSDK_CACHE_CCR_EN_Msk; // Disable cache
-//        while ((CMSDK_CACHE->SR & CMSDK_CACHE_SR_CS_Msk) != (0b00 << CMSDK_CACHE_SR_CS_Pos)); // Wait until cache is disabled
-//        __enable_irq();
+       //Serial.println("Disabling Cache...");
+       __disable_irq();
+       if((CMSDK_CACHE->SR & CMSDK_CACHE_SR_CS_Msk) == (0b10 << CMSDK_CACHE_SR_CS_Pos))
+       CMSDK_CACHE->CCR &=  ~CMSDK_CACHE_CCR_EN_Msk; // Disable cache
+       while ((CMSDK_CACHE->SR & CMSDK_CACHE_SR_CS_Msk) != (0b00 << CMSDK_CACHE_SR_CS_Pos)); // Wait until cache is disabled
+       __enable_irq();
         
         do {
-          //putch(pgm_read_byte_near(address++));
           putch(flash[address++]);
         }
         while (--length);
@@ -279,69 +222,15 @@ void loop() {
 }
 
 uint8_t getch(void){
-
   uint8_t ch;
-
-//  #ifdef SOFT_UART
-//    __asm__ __volatile__(
-//      "1: sbic %[uartPin],%[uartBit]\n"
-//      "   rjmp 1b\n"
-//      "   rcall uartDelay\n"
-//      "2: rcall uartDelay\n"
-//      "   rcall uartDelay\n"
-//      "   clc\n"
-//      "   sbic %[uartPin],%[uartBit]\n"
-//      "   sec\n"
-//      "   dec  %[bitCnt]\n"
-//      "   breq 3f\n"
-//      "   ror  %[ch]\n"
-//      "   rjmp .2b\n"
-//      "3:\n"
-//      :
-//        [ch] "=r" (ch)
-//      :
-//        [bitCnt] "d" (9),
-//        [uartPin] "I" (_SFR_IO_ADDR(UART_PIN)),
-//        [uartBit] "I" (UART_RX_BIT)
-//      :
-//        "r25"
-//        
-//      );
-//
-//      #endif
-while(!Serial.available());
-ch = Serial.read();
+  while(!Serial.available());
+  ch = Serial.read();
+      wdt_reset();
       return ch;
   }
 
-void putch(char ch) {
-//#ifdef SOFT_UART
-//  __asm__ __volatile__(
-//
-//    "   com %[ch]\n"
-//    "   sec\n"
-//    "1: brcc 2f\n"
-//    "   cbi %[uartPort],%[uartBit]\n"
-//    "   rjmp 3f\n"
-//    "2: sbi %[uartPort],%[uartBit]\n"
-//    "   nop\n"
-//    "3: rcall uartDelay\n"
-//    "   rcall uartDelay\n"
-//    "   lsr %[ch]\n"
-//    "   dec %[bitcnt]\n"
-//    "   brne 1b\n"
-//    :
-//    :
-//      [bitcnt] "d" (10),
-//      [ch] "r" (ch),
-//      [uartPort] "I" (_SFR_IO_ADDR(UART_PORT)),
-//      [uartBit] "I" (UART_TX_BIT)
-//    :
-//      "r25"
-// );
-//#endif    
-
-Serial.print(ch);
+void putch(char ch) { 
+  Serial.print(ch);
 }
 
 void getNch(uint8_t count){
